016273e1e72f27aa41066d6d5091233e698207d9
dmaish <mainadaniel81@gmail.com>
2020-10-10 00:41:14 +0300

    ...

--- src/services/baseURL.js
+++ src/services/baseURL.js
@@ -6,5 +6,5 @@ const resolve_base_url = () => {
 }
 export default resolve_base_url;
 
-// rememmber to add /api/v1 preffix
+// rememmber to add /api/v1 preffix.This is the prefix used in the backend routes.
  
